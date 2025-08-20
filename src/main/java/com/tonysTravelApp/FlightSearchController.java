e83049d7f42f570c79819db19f35810bfd9acb57
avblake225 <avblake225@gmail.com>
2025-08-20 16:27:53 +0100

    Successfully parsing test JSON and extracting specific data

--- src/main/java/com/tonysTravelApp/FlightSearchController.java
+++ src/main/java/com/tonysTravelApp/FlightSearchController.java
@@ -10,6 +10,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import org.json.JSONObject;
 
 /**
 * FlightSearchController handles flight search requests using Amadeus API based on user input.
@@ -62,5 +63,60 @@ public class FlightSearchController {
 
           System.out.println(object.get("type").getAsJsonObject().get("name").getAsString());
        } 
+   }
+
+   @GetMapping("/test")
+   public String test()
+   {
+      String input = "{\r\n" + //
+                    "    \"d\": {\r\n" + //
+                    "        \"__metadata\": {\r\n" + //
+                    "            \"type\": \"typehere\",\r\n" + //
+                    "            \"uri\": \"urihere\"\r\n" + //
+                    "        },\r\n" + //
+                    "        \"BOLNR\": \"00170548\",\r\n" + //
+                    "        \"ToCheck_BOL_SO\": {\r\n" + //
+                    "            \"results\": [\r\n" + //
+                    "                {\r\n" + //
+                    "                    \"__metadata\": {\r\n" + //
+                    "                        \"type\": \"typehere\",\r\n" + //
+                    "                        \"uri\":\"urihere\"\r\n" + //
+                    "                    },\r\n" + //
+                    "                    \"DOC_NUM\": \"0011998\",\r\n" + //
+                    "                    \"BOLNR\": \"00263334\",\r\n" + //
+                    "                    \"ToCheck_BOL_SO_DN\": {\r\n" + //
+                    "                        \"results\": [\r\n" + //
+                    "                            {\r\n" + //
+                    "                                \"__metadata\": {\r\n" + //
+                    "                                    \"type\": \"typeheree\",\r\n" + //
+                    "                                    \"uri\": \"urihere\"\r\n" + //
+                    "                                },\r\n" + //
+                    "                                \"DOC_NUM\": \"0011278\",\r\n" + //
+                    "                                \"DEL_NUM\": \"0805137\",\r\n" + //
+                    "                                \"SHIP_NUM\": \"0011716\",\r\n" + //
+                    "                                \"BOLNR\": \"26300777\"\r\n" + //
+                    "                            }\r\n" + //
+                    "                        ]\r\n" + //
+                    "                    }\r\n" + //
+                    "                }\r\n" + //
+                    "            ]\r\n" + //
+                    "        }\r\n" + //
+                    "    }\r\n" + //
+                    "}";
+      
+      String output="";
+
+      JSONObject jsonObject= new JSONObject(input);
+    
+      output=jsonObject.getJSONObject("d")
+                       .getJSONObject("ToCheck_BOL_SO")
+                       .getJSONArray("results")
+                       .getJSONObject(0)
+                       .getJSONObject("ToCheck_BOL_SO_DN")
+                       .getJSONArray("results")
+                       .getJSONObject(0)
+                       .getString("DOC_NUM");
+
+      return "Document Number: "+ output;
    }
 }
