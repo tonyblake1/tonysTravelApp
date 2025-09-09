3ecd6de50c6f05ee4fb2dc6050022466145f0ecd
avblake225 <avblake225@gmail.com>
2025-09-09 18:54:36 +0100

    Added functionality to test dropdown menu

--- src/main/java/com/tonysTravelApp/TestController.java
+++ src/main/java/com/tonysTravelApp/TestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 
 @Controller
@@ -28,5 +29,11 @@ public class TestController {
       model.addAttribute("cityList", cityList);
 
       return "index";
+   }
+
+   @PostMapping(value = "/search")
+   public String save(Model model) {
+      
+      return "searchResults";
    }
 }
